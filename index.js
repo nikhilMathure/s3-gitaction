cafe146abaff4f50d4ff89b6f8b77014695ff398
Nikhil MATHURE <nmathure98@gmail.com>
2024-01-18 00:53:34 +0530

    s3 git action

---
+++ index.js
@@ -0,0 +1,30 @@
+// window.onload = function() {
+//     var x = 1800000,
+//         max = 1805000,
+//         ctr = document.getElementById("counter");
+
+//     function incrementCounter() {
+//        ctr.innerHTML = x;
+//        if (x++ < max)
+//            setTimeout(incrementCounter, 1);
+//     }
+
+//     incrementCounter();
+// };
+
+
+
+// window.onload = function() {
+//     var y = 2000,
+//         may = 3000,
+//         cty = document.getElementById("project");
+
+//     function incrementCounter() {
+//        cty.innerHTML = y;
+//        if (y++ < may)
+//            setTimeout(incrementCounter, 1);
+//     }
+
+//     incrementCounter();
+// };
+
